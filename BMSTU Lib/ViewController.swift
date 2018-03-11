039fb5898a1064831895d234fb58007b9618dfb0
NEWESTERS <ramiro.cruz@mail.ru>
2018-03-11 22:02:26 +0300

    Initial Commit

---
+++ BMSTU Lib/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMSTU Lib
+//
+//  Created by Nikita Balikhin on 11.03.2018.
+//  Copyright © 2018 LandSlide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
